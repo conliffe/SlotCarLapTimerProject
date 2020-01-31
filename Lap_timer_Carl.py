77e933e5c0fd2113af3ffb74925179acc69f9295
conliffe <59636205+conliffe@users.noreply.github.com>
2020-01-30 22:19:04 -0800

    Renamed tons of valiable to have a consistant naming convention in camel case.

--- Lap_timer_Carl.py
+++ Lap_timer_Carl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ########################################################################
-# Filename    : Lap_timer_Carl.py
+# Filename    : lapTimer_Carl.py
 # Description : Reports on the time lapsed between detections of a sensor.
 # For this test code a push button will be the sensor for the lap.
 # The circuit will flash an LED on each lap detection, and green LED to
@@ -15,27 +15,28 @@
 ########################################################################
 
 ###### List of variables ###########
+# fastestLap
 # channel
-# count
 # countDown1, 2, 3, 4, 5
-# fastest_lap
-# green_led
+# getFastestLap
+# greenled
 # i
-# lap_time
+# lapCount
+# lapTime
 # reed
-# red_led
+# redLed
 # reset
 # time
-# time_1
-# time_2
+# previousTime
+# currentTime
 ####################################
 
 ###### List of funcions ############
 # display(time)
-# display_fastest(channel)
-# fastest_flash
-# lap_detect()
-# new_lap(channel)
+# displayFastestLap(channel)
+# fastestLapLEDflash
+# lapDetect()
+# newLap(channel)
 # reset(channel)
 # startSequence()
 ####################################
@@ -59,20 +60,20 @@ GPIO.setmode(GPIO.BCM)
 # pins used for the switches, reed sensor and LEDs
 reset = 16        # GPIO16 pin 36
 reed = 20         # GPIO20 pin 38
-fastest_lap = 21  # GPIO21 pin 40
-red_led = 19      # GPIO19 pin 35, Lap indicator
-green_led = 26    # GPIO21 pin 36, Fastest lap indicator
-countDown5 =  25  # GPIO22 countdown LEDs on bar LED 
-countDown4 =  8   # GPIO8 countdown LEDs on bar LED 
-countDown3 =  7   # GPIO7 countdown LEDs on bar LED 
-countDown2 = 12   # GPIO12 countdown LEDs on bar LED 
-countDown1 = 13   # GPIO13 countdown LEDs on bar LED 
-           
+getFastestLap = 21  # GPIO21 pin 40
+redLed = 19      # GPIO19 pin 35, Lap indicator
+greenLed = 26    # GPIO21 pin 36, Fastest lap indicator
+countDown5 =  25  # GPIO22 countdown LEDs on bar LED
+countDown4 =  8   # GPIO8 countdown LEDs on bar LED
+countDown3 =  7   # GPIO7 countdown LEDs on bar LED
+countDown2 = 12   # GPIO12 countdown LEDs on bar LED
+countDown1 = 13   # GPIO13 countdown LEDs on bar LED
+
 # configure outputs for LED
 print('The LEDs are being configured.  Red for lap detection and green for fasted lap')
 GPIO.setwarnings(False)
-GPIO.setup(red_led, GPIO.OUT)     #Red LED channel 19
-GPIO.setup(green_led, GPIO.OUT)   #Green LED channel 26
+GPIO.setup(redLed, GPIO.OUT)     #Red LED channel 19
+GPIO.setup(greenLed, GPIO.OUT)   #Green LED channel 26
 GPIO.setup(countDown5, GPIO.OUT)  #LED bar LED5 channel 25
 GPIO.setup(countDown4, GPIO.OUT)  #LED bar LED4 channel 8
 GPIO.setup(countDown3, GPIO.OUT)  #LED bar LED3 channel 7
@@ -83,16 +84,16 @@ GPIO.setup(countDown1, GPIO.OUT)  #LED bar LED1 channel 13
 print('Configuring the detection input channels for the GPIO')
 GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # Reset signal channel GPIO16 pin 36
 GPIO.setup(reed, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Reed switch channel GPIO20 pin 38
-GPIO.setup(fastest_lap, GPIO.IN, pull_up_down=GPIO.PUD_UP)    #fastest_lap channel GPIO21 pin 40
+GPIO.setup(getFastestLap, GPIO.IN, pull_up_down=GPIO.PUD_UP)    #getFastestLap channel GPIO21 pin 40
 
 # Switch off LEDs
 print('Switching all LEDs off')
-GPIO.output(red_led, False)
-GPIO.output(green_led, False)
+GPIO.output(redLed, False)
+GPIO.output(greenLed, False)
 
 # Define new variables for lap counting and remembering fastest lap time
-count = 0
-fastest_lap = 99.99
+lapCount = 0
+fastestLap = 9999
 
 # Funtion to run the start sequence countdown
 def startSequence():
@@ -126,22 +127,22 @@ def startSequence():
     print('GO!!!!')
 
 # Function to flash red LED once
-def lap_detect():
+def lapDetect():
 #    print('Lap was detected. Flashing a Red LED')
 #    print('Red LED ON')
-    GPIO.output(red_led, True)
+    GPIO.output(redLed, True)
     time.sleep(0.1)
 #    print('Red LED off')
-    GPIO.output(red_led, False)
+    GPIO.output(redLed, False)
 
 # Function to repeatedly flash green LED when a fastest lap occurs
-def fastest_flash():
+def fastestLapLEDflash():
     for i in range(0,5):
 #        print('Green LED ON')
-        GPIO.output(green_led, True)
+        GPIO.output(greenLed, True)
         time.sleep(0.1)
 #        print('Green LED OFF')
-        GPIO.output(green_led, False)
+        GPIO.output(greenLed, False)
         time.sleep(0.1)
     print('You just completed your fastest lap!')
 
@@ -153,39 +154,39 @@ def display(time):
 #    segment.setColon(True)
 #    segment.writeDigit(3, int(str(time)[2]))
 #    segment.writeDigit(4, int(str(time)[3]))
-    
+
 # Function to determine what actions to take on new lap detection
-def new_lap(channel):
-    lap_detect()
-    global count     # This variable is the lap count.  May want to make this user inputable.
-    global time_1    # This is the previous time or time at start of lap
-    global time_2    # This is the current time or time at end of lap
-    global lap_time  # This is the current lap time 
-    global fastest_lap    # This is the fasted lap time
-    if count < 1:    # This executes on the first lap only to set the start time
-        time_1 = time.time()    # This is the start time and the time at the begining of lap #1
+def newLap(channel):
+    lapDetect()
+    global lapCount        # This variable is the lap count.  May want to make this user inputable.
+    global previousTime       # This is the previous time or time at start of lap
+    global currentTime       # This is the current time or time at end of lap
+    global lapTime     # This is the current lap time
+    global fastestLap   # This is the fasted lap time
+    if lapCount < 1:       # This executes on the first lap only to set the start time
+        previousTime = time.time()    # This is the start time and the time at the begining of lap #1
         print("Lap: " + str(count))
-        count = count + 1   # Increments the lap counter
+        lapCount += 1   # Increments the lap counter
     else:
-        time_2 = time.time()          # Grabs the current time
-        lap_time = time_2 - time_1    # Current time minus previous time gets you the current lap time.
+        currentTime = time.time()          # Grabs the current time
+        lapTime = currentTime - previousTime    # Current time minus previous time gets you the current lap time.
         print(' ')
-        print("Lap: " + str(count), " Lap time = %.3f" % lap_time, ' seconds') # Prints the current lap number and lap time
-        time_1 = time_2     # This sets the lap end time to be the start time of the next lap
-        count = count + 1   # Increments the lap counter
-        if lap_time < fastest_lap:
-            print('New fastest lap!! Lap Time = ' "%.3f" % lap_time, ' seconds') # Prints the fasted lap time when it happens
-            fastest_flash() # Calls function that lights the green LED ofr fastest lap indicator
-            fastest_lap = lap_time    # Serts a new fasted lap standard to hit
-        #if lap_time < 10:   # DOT SURE WHY IT ONLY CALLS & SEGMENT WHEN LAP TIME IS LESS THAN 10 SEC
-        display(lap_time)    # Calls function that sisplays lap time on 7 Segment display
+        print("Lap: " + str(count), " Lap time = %.3f" % lapTime, ' seconds') # Prints the current lap number and lap time
+        previousTime = currentTime     # This sets the lap end time to be the start time of the next lap
+        lapCount += 1   # Increments the lap counter
+        if lapTime < fastestLap:
+            print('New fastest lap!! Lap Time = ' "%.3f" % lapTime, ' seconds') # Prints the fasted lap time when it happens
+            fastestLapLEDflash() # Calls function that lights the green LED ofr fastest lap indicator
+            fastestLap = lapTime    # Serts a new fasted lap standard to hit
+        #if lapTime < 10:   # DOT SURE WHY IT ONLY CALLS & SEGMENT WHEN LAP TIME IS LESS THAN 10 SEC
+        display(lapTime)    # Calls function that sisplays lap time on 7 Segment display
 
 # Function to reset lap times and counts
 def reset(channel):
     global count
-    global fastest_lap
-    count = 0
-    fastest_lap = 99999
+    global fastestLap
+    lapCount = 0
+    fastestLap = 99999
     print('Lap times reset to zero')
 #    segment.writeDigit(0, 0)
 #    segment.writeDigit(1, 0)
@@ -193,25 +194,25 @@ def reset(channel):
 #    segment.writeDigit(4, 0)
 
 # Function to write the fastest lap time to the display
-def display_fastest(channel):
-    global fastest_lap
-    print('Displaying fastest lap to 7 segment display.  Fastest lap = ' "%.3f" % fastest_lap)
-#    segment.writeDigit(1, int(str(fastest_lap)[0]))
+def displayFastestLap(channel):
+    global fastestLap
+    print('Displaying fastest lap to 7 segment display.  Fastest lap = ' "%.3f" % fastestLap)
+#    segment.writeDigit(1, int(str(fastestLap)[0]))
 #    segment.setColon(True)
-#    segment.writeDigit(3, int(str(fastest_lap)[2]))
-#    segment.writeDigit(4, int(str(fastest_lap)[3]))
+#    segment.writeDigit(3, int(str(fastestLap)[2]))
+#    segment.writeDigit(4, int(str(fastestLap)[3]))
 
 startSequence()   # Calls the start sequence
 GPIO.add_event_detect(16, GPIO.FALLING, callback=reset, bouncetime=200) # This is reset
-GPIO.add_event_detect(20, GPIO.FALLING, callback=new_lap, bouncetime=2000) # The is new lap
-GPIO.add_event_detect(21, GPIO.FALLING, callback=display_fastest, bouncetime=200) # This is display fasted lap
-  
+GPIO.add_event_detect(20, GPIO.FALLING, callback=newLap, bouncetime=2000) # The is new lap
+GPIO.add_event_detect(21, GPIO.FALLING, callback=displayFastestLap, bouncetime=200) # This is display fasted lap
+
 try:
-    
+
     while True:
         time.sleep(0.01)
-        pass
-    
+        pass    # Does nothing.  Kind of like a no op
+
 finally:
     print('Done!!  The Race is over.')
 #    segment = SevenSegment(address=0x70)
